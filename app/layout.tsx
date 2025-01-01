bf6af871244afbf2a1eaef99340ad32c9223d67f
Sadiq Umar <umarsadiqibrahim@gmail.com>
2025-01-01 11:36:25 +0100

    feature: add sharing preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     description: 'Welcome to my portfolio site. Get to know me professionally',
     images: [
       {
-        url: 'https://sadiqumar.vercel.app/icons/code-circle.svg',
+        url: 'https://sadiqumar.vercel.app/profile/headshot.png',
         width: 1200,
         height: 630,
         alt: 'Preview image description',

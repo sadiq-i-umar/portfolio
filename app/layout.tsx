3358dde49a5053f133a63561ff768076a28ca3bb
Sadiq Umar <umarsadiqibrahim@gmail.com>
2025-01-05 17:40:26 +0100

    Change openGraph title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,31 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: 'Sadiq Ibrahim Umar - Software Engineer | Web Developer',
-  description: 'Welcome to my portfolio site. Get to know me professionally',
-  creator: 'Sadiq Ibrahim  Umar',
+  title: "Sadiq Ibrahim Umar - Software Engineer | Web Developer",
+  description: "Welcome to my portfolio site. Get to know me professionally",
+  creator: "Sadiq Ibrahim  Umar",
   openGraph: {
-    title: 'Sadiq Ibrahim Umar - Software Engineer',
-    description: 'Welcome to my portfolio site. Get to know me professionally',
+    title: "Sadiq Ibrahim Umar - Software Engineer | Web Developer",
+    description: "Welcome to my portfolio site. Get to know me professionally",
     images: [
       {
-        url: 'https://sadiqumar.vercel.app/profile/headshot.png',
+        url: "https://sadiqumar.vercel.app/profile/headshot.png",
         width: 1200,
         height: 630,
-        alt: 'Preview image description',
+        alt: "Preview image description",
       },
     ],
   },
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
